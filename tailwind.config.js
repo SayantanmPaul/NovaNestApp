147f75b6e16eabbfcd8c8752a8c9f1077821c7f5
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-11-23 03:25:00 +0530

    custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,18 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: { 
       fontFamily:{
         raleway: [ 'Raleway', "sans-serif"],
+      },
+      //custom color inputs
+      colors:{
+        'black-bg': 'rgba(17, 25, 40, 0.75)',
+        'light-bg': 'rgba(255, 255, 255, 0.75);',
+        'black-border':'rgba(255, 255, 255, 0.125);',
+        'light-border':'rgba(209, 213, 219, 0.3);'
       }
     },
   },
